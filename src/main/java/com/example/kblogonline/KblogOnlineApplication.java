65ee79814d1d21118a869a4803aaf1e9ba6bca4e
kane <617591257@qq.com>
2021-07-21 17:15:09 +0800

    1.1 项目初始化

---
+++ src/main/java/com/example/kblogonline/KblogOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.kblogonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KblogOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KblogOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kblogonline/KblogOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kblogonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KblogOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
